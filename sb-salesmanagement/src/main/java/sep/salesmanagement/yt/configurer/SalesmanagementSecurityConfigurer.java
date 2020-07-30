3f91e06ad48372cf0bddeda612eaba7f245de3d8
yamaguchitomofumi119 <yamaguchitomofumi119@DESKTOP-F8OC520.ad.se-project.co.jp>
2020-07-30 17:57:08 +0900

    Spring Boot Salesmanagement at SEP 2020-07-30

--- sb-salesmanagement/src/main/java/sep/salesmanagement/yt/configurer/SalesmanagementSecurityConfigurer.java
+++ sb-salesmanagement/src/main/java/sep/salesmanagement/yt/configurer/SalesmanagementSecurityConfigurer.java
@@ -1,17 +1,40 @@
 package sep.salesmanagement.yt.configurer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import sep.salesmanagement.yt.service.SalesManagementUserDetailService;
+
+@SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
 public class SalesmanagementSecurityConfigurer extends WebSecurityConfigurerAdapter {
+	@Bean
+    PasswordEncoder passwordEncoder() {
+        //ハッシュ化無し(非推奨)
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+	@Override
+	public void configure(WebSecurity webSecurity) throws Exception {
+
+	}
+
+	@Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeRequests()
-        //未ログイン時でもアクセスできるURIの指定(つまり、ログインページ)
+        //未ログイン時でもアクセスできるURIの指定
                 .mvcMatchers("/salesmanagement/login").permitAll()
+        //CSS, JavaScriptファイルのアクセス許可
+                .antMatchers("/css/**","/js/**").permitAll()
         //未ログイン時のログインページ以外へのアクセスを許可しない
                 .anyRequest().authenticated()
                 .and()
@@ -19,8 +42,21 @@ public class SalesmanagementSecurityConfigurer extends WebSecurityConfigurerAdap
                 .csrf().disable();
 
         httpSecurity.formLogin()
+        //ログイン認証に使うaction:URI
                 .loginProcessingUrl("/salesmanagement/login")
+        //ログインページURI
                 .loginPage("/salesmanagement/login")
+                //ログインページの<input>のname属性
+                .usernameParameter("username")
+                .passwordParameter("password")
+        //ログイン認証成功時のデフォルト遷移先URI
                 .defaultSuccessUrl("/salesmanagement/order_list");
+
+    }
+
+    protected static class AuthenticationConfiguration extends GlobalAuthenticationConfigurerAdapter {
+    	@Autowired
+    	SalesManagementUserDetailService salesManagementUserDetailService;
+
     }
 }
---
+++ sb-salesmanagement/src/main/java/sep/salesmanagement/yt/service/SalesManagementUserDetailService.java
@@ -0,0 +1,15 @@
+package sep.salesmanagement.yt.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class SalesManagementUserDetailService implements UserDetailsService {
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+}
